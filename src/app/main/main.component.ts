f3b095c37e89d5bb79281bc60fc8b478d6a198fb
Omar Jaw <omarjawhar.10@gmail.com>
2020-10-04 07:34:38 +0300

    UI tweaks

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NbSidebarService} from '@nebular/theme';
 import {Router} from '@angular/router';
 
@@ -9,7 +9,9 @@ import {Router} from '@angular/router';
 })
 export class MainComponent implements OnInit {
 
-  constructor(private sidebarService: NbSidebarService, private router: Router) { }
+
+  constructor(private sidebarService: NbSidebarService, private router: Router) {
+  }
 
   ngOnInit(): void {
     this.router.navigate(['start']);
